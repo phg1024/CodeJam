0389fe6afd7ab02d2dec5c355255a5624f3f3717
Peihong Guo <peihongguo@gmail.com>
2014-01-09 18:09:39 -0600

    better structured

--- 2008/Round1/ProblemA.py
+++ 2008/Round1/ProblemA.py
@@ -1,5 +1,10 @@
 __author__ = 'Peihong'
 
+def dot(a, b):
+    c = zip(a, b)
+    d = map(lambda (x, y): x*y, c)
+    return reduce(lambda x, y: x+y, d, 0)
+
 if __name__ == "__main__":
     f = open('A-large-practice.in', 'r')
     fout = open('A-large-result.out', 'w')
@@ -11,9 +16,8 @@ if __name__ == "__main__":
 
         a = sorted(a, lambda x, y: x-y)
         b = sorted(b, lambda x, y: y-x)
-        c = zip(a, b)
-        d = map(lambda (x, y): x*y, c)
-        res = reduce(lambda x, y: x+y, d, 0)
+
+        res = dot(a, b)
         print 'Case #%d: %d' % (i+1, res)
         fout.write('Case #%d: %d\n' % (i+1, res))
     f.close()
